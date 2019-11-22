d64af91150bcc6f25e27d82df35964f163ba62e7
ggonzalez <gedgonz7@gmail.com>
2019-11-22 11:34:37 -0600

    Se agrego controlado Producto para la salidas Json

---
+++ BackendProductApi/Controllers/ProductoController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendProductApi.Aplicacion.Contratos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : Controller
+    {
+        private readonly IProductoAplicacionServicio productoAplicacionServicio;
+
+        public ProductoController(IProductoAplicacionServicio productoAplicacionServicio)
+        {
+            this.productoAplicacionServicio = productoAplicacionServicio;
+        }
+        // GET: api/Producto
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await productoAplicacionServicio.obtenerListProducto();
+
+            return Json(result);
+        }
+
+        // GET: api/Producto/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Producto
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Producto/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
