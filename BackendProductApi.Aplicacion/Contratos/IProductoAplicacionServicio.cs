8a4d8403e72f21f6d2e4a0a55721ae23992610ce
ggonzalez <gedgonz7@gmail.com>
2019-11-22 11:09:47 -0600

    Consumiendo Servicios del domino y hacendo maping a las dto

---
+++ BackendProductApi.Aplicacion/Contratos/IProductoAplicacionServicio.cs
@@ -0,0 +1,18 @@
+﻿using BackendProductApi.Aplicacion.EntidadesDto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendProductApi.Aplicacion.Contratos
+{
+    public interface IProductoAplicacionServicio
+    {
+        public Task<List<ProductoDto>> obtenerListProducto();
+        public Task<ProductoDto> obtenerProducto(int id);
+
+        public Task guardarProducto(ProductoDto producto);
+        public Task actualizarProducto(ProductoDto producto);
+        public Task eliminarProducto(int id);
+    }
+}
---
+++ BackendProductApi.Aplicacion/Servicios/ProductoAplicacionServicio.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using BackendProductApi.Aplicacion.Contratos;
+using BackendProductApi.Aplicacion.EntidadesDto;
+using BackendProductApi.Dominio.Servicio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendProductApi.Aplicacion.Servicios
+{
+    public class ProductoAplicacionServicio : IProductoAplicacionServicio
+    {
+        private readonly IProductoDominioServicio productoDominioServicio;
+        private readonly IMapper _mapper;
+        public ProductoAplicacionServicio(IProductoDominioServicio productoDominioServicio,IMapper _mapper)
+        {
+            this.productoDominioServicio = productoDominioServicio;
+            this._mapper = _mapper;
+        }
+        public Task actualizarProducto(ProductoDto producto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task eliminarProducto(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task guardarProducto(ProductoDto producto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ProductoDto>> obtenerListProducto()
+        {
+            var productos = await productoDominioServicio.obtenerListProducto();
+            var productosdto = _mapper.Map<List<ProductoDto>>(productos);
+
+            return productosdto;
+        }
+
+        public Task<ProductoDto> obtenerProducto(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
