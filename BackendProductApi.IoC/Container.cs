4677f0eb9001c25092769a6c36064baa64e77372
ggonzalez <gedgonz7@gmail.com>
2019-11-22 15:12:19 -0600

    Se agrego IoC para la inyeccion de dependencia y dividirlo del StartUp

---
+++ BackendProductApi.IoC/Container.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendProductApi.IoC
+{
+    public class Container
+    {
+        public static void  Servicios(IServiceCollection servicio) 
+        {
+        
+        }
+    }
+}
