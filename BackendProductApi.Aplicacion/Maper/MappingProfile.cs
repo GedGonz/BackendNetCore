bade3cd01f61fe90eb39a329aaba6d966aa2fa67
ggonzalez <gedgonz7@gmail.com>
2019-11-22 10:50:16 -0600

    Configurando Mapping para el uso de Dtos

---
+++ BackendProductApi.Aplicacion/Maper/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BackendProductApi.Aplicacion.EntidadesDto;
+using BackendProductApi.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendProductApi.Aplicacion.Maper
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Producto, ProductoDto>()
+            .ForMember(x=>x.ProductoId, ops=>ops.MapFrom(x=>x.ProductoId))
+            .ForMember(x => x.Nombre, ops => ops.MapFrom(x => x.Nombre))
+            .ForMember(x => x.Descripcion, ops => ops.MapFrom(x => x.Descripcion))
+            .ForMember(x => x.Categoria, ops => ops.MapFrom(x => x.Categoria))
+            .ForMember(x => x.Precio, ops => ops.MapFrom(x => x.Precio))
+            .ForMember(x => x.Imagen, ops => ops.MapFrom(x => x.Imagen)).ReverseMap();
+        }
+    }
+}
