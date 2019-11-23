c21b423f208316f185868cc3297d85d33a6f4ab4
Gerald Gonzalez <gedgonz7@gmail.com>
2019-11-23 11:19:50 -0600

    Se agrego la configuracion del en el StarUp para la Inyeccion de dependencias y el Maper

---
+++ BackendProductApi.IoC/ContainerIoC.cs
@@ -0,0 +1,26 @@
+﻿using BackendProductApi.Aplicacion.Contratos;
+using BackendProductApi.Aplicacion.Servicios;
+using BackendProductApi.Dominio.Contratos;
+using BackendProductApi.Dominio.Dominio;
+using BackendProductApi.Dominio.Servicio;
+using BackendProductApi.Infraestructura.Repositorio;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendProductApi.IoC
+{
+    public static class ContainerIoC
+    {
+        public static IServiceCollection Servicios(IServiceCollection servicio) 
+        {
+            servicio.AddScoped<IProductoRepositorio, ProductoRepositorio>();
+            servicio.AddScoped<IProductoDominioServicio, ProductoDominioServicio>();
+            servicio.AddScoped<IProductoAplicacionServicio, ProductoAplicacionServicio>();
+
+            return servicio;
+
+        }
+    }
+}
--- BackendProductApi/Startup.cs
+++ BackendProductApi/Startup.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using BackendProductApi.Infraestructura.Contexto;
+using BackendProductApi.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +30,16 @@ namespace BackendProductApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            //services.AddControllers();
+
+
+            services.AddDbContext<ProductoContexto>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("ProductoDB")));
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddAutoMapper(typeof(Startup));
+            ContainerIoC.Servicios(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
